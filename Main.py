feddc27640691c513062981faf3bb7b0c6b72f1e
URK21CS2020AKASHNAIR <109648018+URK21CS2020AKASHNAIR@users.noreply.github.com>
2022-09-07 10:33:29 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -100,18 +100,34 @@ class DoublyCircularLinkedList:
 # Do not change the following code
 operations = []
 for specific_operation in input().split(','):
-    operations.append(specific_operation.strip())
+ operations.append(specific_operation.strip())
 input_data = input()
 data = []
 iteration_count = 0
 
 for item in input_data.split(', '):
-    inner_list = []
-    if item.isnumeric():
-        data.append(int(item))
-    elif item.startswith('['):
-        item = item[1:-1]
-        for letter in item.split(','):
-            if letter.isnumeric():
-                inner_list.append(int(letter))
-        data.append
+ inner_list = []
+ if item.isnumeric():
+ data.append(int(item))
+ elif item.startswith('['):
+ item = item[1:-1]
+ for letter in item.split(','):
+ if letter.isnumeric():
+ inner_list.append(int(letter))
+ data.append(inner_list)
+obj = DoublyCircularLinkedList()
+result = []
+for i in range(len(operations)):
+ if operations[i] == "add_at_head":
+ result.append(obj.add_at_head(data[i]))
+ elif operations[i] == "add_at_tail":
+ result.append(obj.add_at_tail(data[i]))
+ elif operations[i] == "add_at_index":
+ result.append(obj.add_at_index(int(data[i][0]), data[i][1]))
+ elif operations[i] == "get":
+ result.append(obj.get(data[i]))
+ elif operations[i] == "get_previous_next":
+ result.append(obj.get_previous_next(data[i]))
+ elif operations[i] == 'delete_at_index':
+ result.append(obj.delete_at_index(data[i]))
+print(result)
